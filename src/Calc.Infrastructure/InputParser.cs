4f656b5481c0a2e1c1e1348c90a38952f0f23bec
Your Name <you@example.com>
2024-09-09 15:00:22 -0700

    feat: Implement custom delimiter parsing in InputParser

---
+++ src/Calc.Infrastructure/InputParser.cs
@@ -0,0 +1,36 @@
+using Calc.Core.Interfaces;
+
+public class InputParser
+{
+    private readonly IDelimiterStrategyFactory _delimiterStrategyFactory;
+    private readonly IDefaultDelimiterStrategy _defaultStrategy;
+
+    public InputParser(IDelimiterStrategyFactory delimiterStrategyFactory, IDefaultDelimiterStrategy defaultStrategy)
+    {
+        _delimiterStrategyFactory = delimiterStrategyFactory;
+        _defaultStrategy = defaultStrategy;
+    }
+
+    public string[] Parse(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return new string[0];
+        }
+
+        if (input.StartsWith("//"))
+        {
+            int newLineIndex = input.IndexOf('\n');
+            if (newLineIndex > 2 && newLineIndex < input.Length - 1)
+            {
+                string customDelimiter = input.Substring(2, newLineIndex - 2);
+                string numberString = input.Substring(newLineIndex + 1);
+                var strategy = _delimiterStrategyFactory.CreateStrategy(customDelimiter);
+                return strategy.Split(numberString);
+            }
+        }
+
+        // Use default strategy for all other cases
+        return _defaultStrategy.Split(input);
+    }
+}
